ef3f9f0c40fc30754955d66e4a056d1f06829e6c
Raphael-Soares <soaresphaell@gmail.com>
2023-02-06 10:29:13 -0300

    Project setup

---
+++ App.jsx
@@ -0,0 +1,10 @@
+import {StyleSheet, Text, View} from "react-native";
+import React from "react";
+import Home from "./src/pages/Home";
+
+function App() {
+    return <Home />;
+}
+
+export default App;
+const styles = StyleSheet.create({});
